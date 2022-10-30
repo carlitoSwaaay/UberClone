d986b394215d05c34e94d026de2bc17f670959dd
carlitoSwaaay <cacollier02@yahoo.com>
2022-10-30 15:04:01 -0700

    added react redux

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+//set up redux
 
 export default function App() {
   return (
