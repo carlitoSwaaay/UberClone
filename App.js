8d3edf4f5639bed701bd42d4cf9c14ce3c47ce94
carlitoSwaaay <cacollier02@yahoo.com>
2022-10-30 15:35:42 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 //set up redux
 
 export default function App() {
   return (
-    <Provider>  
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Hello world</Text>
       </View>
