7fcf4d2e60a6236e6e7ac5e4b4fa38a619b2c6ae
carlitoSwaaay <cacollier02@yahoo.com>
2022-10-30 17:34:02 -0700

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>i am the HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text>I am the HomeScreen</Text>
+    </SafeAreaView>
   )
 }
 
