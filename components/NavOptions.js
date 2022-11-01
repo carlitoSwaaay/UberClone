715ea3201deb95929c001e048ccf4456f3ef0060
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-01 11:20:16 -0700

    Update NavOptions.js

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,20 +1,48 @@
-import { Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { Text, View, TouchableOpacity, FlatList, Image } from 'react-native';
 import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+
+
+const data = [
+  {
+    id: "123",
+    title: "Get a Ride",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcfkvzOKeeO_wELzeG83cViDABdyc2LNHQvc7vZogE0fytt4s5-Ytxxkoim3xaS7A1-Sg&usqp=CAU",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order Food",
+    image: "https://www.iconbolt.com/preview/twitter/font-awesome-solid/utensils.svg",
+    screen: "EatsScreen",
+  },
+
+];
 
 
 const NavOptions = () => {
   return (
     <FlatList
       data={data}
+      keyExtractor={(item) => item.id}
       horizontal
+      showsHorizontalScrollIndicator={false}
       renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>{item.title}</Text>
+        <TouchableOpacity
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120 }}
+              source={{ uri: item.image }}
+            />
+            <Text>{item.title}</Text>
+          </View>
         </TouchableOpacity>
       )}
     />
   );
 };
 
-export default NavOptions
+export default NavOptions;
 
