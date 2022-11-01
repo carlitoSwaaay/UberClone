905a405163815081529521c23c4e592d98c5e7f6
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-01 14:41:06 -0700

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { View, SafeAreaView, Image } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 
 
