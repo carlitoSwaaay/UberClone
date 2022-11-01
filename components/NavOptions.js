a228f01b3bb2503940ebf9b24fbcc697c64bf790
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-01 13:43:06 -0700

    navoptions updated

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -32,6 +32,7 @@ const NavOptions = () => {
       showsHorizontalScrollIndicator={false}
       renderItem={({ item }) => (
         <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
           style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
         >
           <View>
