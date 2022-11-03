fbb93d5ced6ec268755e65ad24cfd82351aedc2e
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-03 14:44:03 -0700

    Update Map.js

---
+++ components/Map.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import MapView, { Marker } from 'react-native-maps/lib/MapView';
+
+const Map = () => {
+  return (
+    <MapView
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  );
+};
+
+export default Map
+
+const styles = StyleSheet.create({})
