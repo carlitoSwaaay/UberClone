a1ee1202884ba33bb48413458af01ab302a96208
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-03 15:02:54 -0700

    added map (GG)

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -7,13 +7,14 @@ import Map from '../components/Map';
 const MapScreen = () => {
   return (
     <View>
-
-      <View style={tw`h-1/2`}><Map /></View>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
 
       <View style={tw`h-1/2`}></View>
     </View>
-  )
-}
+  );
+};
 
 export default MapScreen;
 
