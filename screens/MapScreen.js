c06aa1f450afb7f4df2e59260d25e25757ca0997
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-06 10:37:15 -0800

    updated navOptions and MapScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,16 +2,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 const MapScreen = () => {
+  const Stack = createStackNavigator();
   return (
     <View>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
 
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+
+      </View>
     </View>
   );
 };
