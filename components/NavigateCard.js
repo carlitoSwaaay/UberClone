b6c642d9f1eddf24923df98be488868c6e1bfb02
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-06 11:42:18 -0800

    Update NavigateCard.js

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -5,13 +5,12 @@ import tw from 'tailwind-react-native-classnames';
 const NavigateCard = () => {
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
-
-      <Text>nav card here</Text>
-
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning Carlos!</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}></View>
     </SafeAreaView>
   );
 };
 
-export default NavigateCard
+export default NavigateCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
