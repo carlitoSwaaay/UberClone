8fb6f80ffe88e0a2aa5b5824614b238e04e73361
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-08 10:13:54 -0800

    Update NavigateCard.js

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -9,7 +9,13 @@ const NavigateCard = () => {
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Good Morning Carlos!</Text>
       <View style={tw`border-t border-gray-200 flex-shrink`}>
-
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder='Where to?'
+            nearbyPlacesAPI='GooglePlacesSearch'
+            debounce={400}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
