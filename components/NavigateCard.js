149ddcfe0053ad126d3e5fd9531024e7aa7178ca
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-08 10:11:53 -0800

    Update NavigateCard.js

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const NavigateCard = () => {
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Good Morning Carlos!</Text>
-      <View style={tw`border-t border-gray-200 flex-shrink`}></View>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+
+      </View>
     </SafeAreaView>
   );
 };
