c06e0fd3dab899e594e150181d67a83a341a540d
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-08 12:26:23 -0800

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -2,9 +2,9 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
-import { selectOrigin, selectDestination } from '../slices/navSlice';
 import { useSelector } from 'react-redux';
 import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
