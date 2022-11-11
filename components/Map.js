6b143845fd833f868cfca942ae1401dcc6c004e4
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-11 10:55:57 -0800

    Update Map.js
    added destination pin, and re-added useEffect hook since redux slice was debugged

--- components/Map.js
+++ components/Map.js
@@ -13,21 +13,21 @@ import MapViewDirections from 'react-native-maps-directions';
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
-  // const mapRef = useRef(null);
+  const mapRef = useRef(null);
 
-  // useEffect(() => {
-  //   if (!origin || !destination) return;
+  useEffect(() => {
+    if (!origin || !destination) return;
 
   //   //Zoom & fit to markers
-  //   mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
-  //     edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
-  //   });
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
 
-  // }, [origin, destination]);
+  }, [origin, destination]);
 
   return (
     <MapView
-      // ref={mapRef}
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
@@ -60,6 +60,20 @@ const Map = () => {
           identifier="origin"
         />
       )}
+      {
+        destination?.location && (
+          <Marker
+            coordinate={{
+              latitude: destination.location.lat,
+              longitude: destination.location.lng,
+
+            }}
+            title="Destination"
+            description={destination.description}
+            identifier="destination"
+          />
+        )
+      }
     </MapView>
   );
 };
