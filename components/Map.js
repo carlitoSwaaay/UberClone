52020c39275ded0d7f9771f4850cf5964425f1a4
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-11 11:04:49 -0800

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -20,7 +20,7 @@ const Map = () => {
 
   //   //Zoom & fit to markers
     mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
-      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+      edgePadding: { top: 70, right: 70, bottom: 70, left: 70 },
     });
 
   }, [origin, destination]);
