9e5c07c2fce3f51b2ebf4fcbca6d4be7bcf3dae7
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-11 11:18:29 -0800

    Update NavFavorites.js

--- components/NavFavorites.js
+++ components/NavFavorites.js
@@ -1,13 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { Icon } from 'react-native-elements';
+
+const data = [
+  {
+    id: "123",
+    icon: "home",
+    location: "Home",
+    destination: "Code Street, London, UK",
+  },
+  {
+    id: "456",
+    icon: "briefcase",
+    location: "Work",
+    destination: "London Eye, London, UK",
+
+  },
+];
 
 const NavFavorites = () => {
+
   return (
-    <View>
-      <Text>NavFavorites!!</Text>
-    </View>
-  )
-}
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity>
+          <Text>YO</Text>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
 
 export default NavFavorites
 
