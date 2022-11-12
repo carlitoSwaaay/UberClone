e07c52684a2c73da012c6e43998f063dab73f8db
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-12 11:01:44 -0800

    Update RideOptionsCard.js

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,14 +1,23 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
-      <Text
-        style={tw`text-center py-5 text-xl`}
-      >Select a Ride
-      </Text>
+      <View>
+        <TouchableOpacity onPress={() => navigation.navigate("NavigationCard")}
+          style={tw`absolute top-3 left-5 p-3 rounded-full`}>
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text
+          style={tw`text-center py-5 text-xl`}
+        >Select a Ride
+        </Text>
+      </View>
     </SafeAreaView>
   )
 }
