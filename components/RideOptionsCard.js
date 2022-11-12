0107904d7a7d8d90dcc2f307d0d25e9b09d4183a
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-12 11:10:55 -0800

    Update RideOptionsCard.js

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -9,8 +9,10 @@ const RideOptionsCard = () => {
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
-        <TouchableOpacity onPress={() => navigation.navigate("NavigateCard")}
-          style={tw`absolute top-3 left-5 p-3 rounded-full`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
         <Text
@@ -19,8 +21,8 @@ const RideOptionsCard = () => {
         </Text>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default RideOptionsCard;
 
