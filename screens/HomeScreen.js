6dadd0da7a06f2ea32412f01279d5090dd1223fd
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-16 15:08:49 -0800

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, SafeAreaView, Image } from 'react-native';
+import { View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
@@ -7,15 +7,24 @@ import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from 'react-redux';
 import { setDestination, setOrigin } from '../slices/navSlice';
 import NavFavorites from '../components/NavFavorites';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 
 
 const HomeScreen = () => {
   const dispach = useDispatch();
+  const navigation = useNavigation();
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("ProfileScreen")}
+          style={tw`bg-gray-100 absolute top-9 left-8 z-50 p-3 rounded-full shadow-lg`}
+        >
+          <Icon name="menu" />
+        </TouchableOpacity>
         <Image style={{ width: 120, height: 120, resizeMode: "contain", alignSelf: "flex-end" }}
           source={{
             uri: "https://mma.prnewswire.com/media/1671139/Uber_Logo.jpg?w=400",
