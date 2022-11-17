ed986412d8148d503ba5491d3c9c6a7d354269c2
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-17 11:02:41 -0800

    MOVED MENUBUTTON TO COMPONENTS FOLDER

---
+++ components/MenuButton.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MenuButton = () => {
+  return (
+    <View>
+      <Text>MenuButton</Text>
+    </View>
+  )
+}
+
+export default MenuButton
+
+const styles = StyleSheet.create({})
