200ad53ea50ed696b8d17dcb0012279fc5b3f62b
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-17 12:39:46 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { KeyboardAvoidingView, Platform } from 'react-native';
+import { KeyboardAvoidingView, Platform, TouchableOpacity, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -11,8 +11,20 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Button } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
+import tw from 'tailwind-react-native-classnames';
 
+const MenuButton = () => {
+  return (
+    <View >
+      <TouchableOpacity
+        style={tw`bg-gray-100 absolute top-9 left-8 z-50 p-3 rounded-full shadow-lg`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
+    </View >
+  );
+};
 
 export default function App() {
 
@@ -28,6 +40,7 @@ export default function App() {
             behavior={Platform.OS === "ios" ? "padding" : "height"}
             keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
           >
+            <MenuButton />
             <Drawer.Navigator>
               {/* <Stack.Navigator> */}
               <Drawer.Screen //Stack.Screen
