378d4a107200607999e8b203b89732ebe7474b90
carlitoSwaaay <cacollier02@yahoo.com>
2022-11-17 11:09:14 -0800

    Update MenuButton.js

--- components/MenuButton.js
+++ components/MenuButton.js
@@ -1,6 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+const data = [
+  {
+    id: "123",
+    icon: "home",
+    location: "Home",
+    destination: "Code Street, London, UK", //link to profile screen
+  },
+  {
+    id: "456",
+    icon: "briefcase",
+    location: "Work",
+    destination: "London Eye, London, UK", //link to profile screen
+
+  },
+]
+
 const MenuButton = () => {
   return (
     <View>
